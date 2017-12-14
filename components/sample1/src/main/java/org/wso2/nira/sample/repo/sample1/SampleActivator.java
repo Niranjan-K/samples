70c5e18384a5d58f043f0c45e575f398543deee4
Niranjan-K <niranjan.karu@gmail.com>
2017-12-14 16:41:35 +0530

    update

---
+++ components/sample1/src/main/java/org/wso2/nira/sample/repo/sample1/SampleActivator.java
@@ -0,0 +1,28 @@
+package org.wso2.nira.sample.repo.sample1;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by nira on 12/14/17.
+ */
+public class SampleActivator implements BundleActivator {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleActivator.class);
+
+    @Activate
+    public void start(BundleContext bundleContext) throws Exception {
+        logger.info("Testing one two three");
+    }
+
+    @Deactivate
+    public void stop(BundleContext bundleContext) throws Exception {
+        logger.debug("Deactivating startup resolver component available in bundle {}",
+                bundleContext.getBundle().getSymbolicName());
+    }
+}
